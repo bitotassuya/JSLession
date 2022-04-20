16c52bc35f9c97bdf1732dd93a48475ac7032058
bitotassuya <jareansong@gmail.com>
2022-04-20 22:40:30 +0700

    FIndexReturn

--- EP15_Array_Filter.js
+++ EP15_Array_Filter.js
@@ -1,11 +1,13 @@
 //Array Filter
-const data =[
-    {name:"นนท์",salary:25000,department:"โปรแกรมเมอร์"},
-    {name:"มาย",salary:10000,department:"เด็กฝึกงาน"},
-    {name:"เม",salary:40000,department:"ฝ่ายขาย"},
-    {name:"หมู",salary:32000,department:"ที่ปรึกษาโปรเตค"},
-    {name:"แบงค์",salary:15000,department:"โปรแกรมเมอร์"}
-]
-const result = data.filter(element => element.salary >10000).filter(element => element.department === "โปรแกรมเมอร์")
+const data = [
+  { name: "นนท์", salary: 25000, department: "โปรแกรมเมอร์" },
+  { name: "มาย", salary: 10000, department: "เด็กฝึกงาน" },
+  { name: "เม", salary: 40000, department: "ฝ่ายขาย" },
+  { name: "หมู", salary: 32000, department: "ที่ปรึกษาโปรเตค" },
+  { name: "แบงค์", salary: 15000, department: "โปรแกรมเมอร์" },
+];
+const result = data
+  .filter((element) => element.salary > 10000)
+  .filter((element) => element.department === "โปรแกรมเมอร์");
 
 console.log(result);
---
+++ EP17_Find_Index.js
@@ -0,0 +1,11 @@
+// Find Index
+const numbers = [4, 9, 16, 25, 29];
+
+/*console/log(`First number over 18 has index  ${numbers.findIndex(myFunction)}` */
+const DataIndex = numbers.findIndex((value) => value > 18);
+
+console.log(`First number over 18 has index ${DataIndex}`);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
