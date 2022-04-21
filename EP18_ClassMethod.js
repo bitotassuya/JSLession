745f029842cf5e8e6e3d77d283a3e09bd2cf0005
bitotassuya <jareansong@gmail.com>
2022-04-21 22:10:34 +0700

    class method

---
+++ EP18_ClassMethod.js
@@ -0,0 +1,14 @@
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age(x) {
+    return x - this.year;
+  }
+}
+let date = new Date();
+let year = date.getFullYear();
+
+let myCar = new Car("Ford", 2012);
+console.log(`My car is ${myCar.age(year)} years old.`);
